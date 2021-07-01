97c0ef478165898aa3954c9aceb3990b09248ecf
gapspace@engineer.com <1Biggithubacount!>
2021-07-01 11:10:32 +0100

    Setting up the connection, request, and Rest controller.

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,13 +1,66 @@
 package com.example.demo;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 @SpringBootApplication
-public class DemoApplication {
+public class DemoApplication implements CommandLineRunner {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
 
+    @Override
+    public void run(String... args) throws Exception {
+        URL url = new URL("https://reference.intellisense.io/test.dataprovider");
+        HttpURLConnection con = (HttpURLConnection) url.openConnection();
+        con.setRequestMethod("GET");
+        int status = con.getResponseCode();
+        System.out.println(status);
+
+        BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
+        String inputLine;
+        StringBuffer content = new StringBuffer();
+        while ((inputLine = in.readLine()) != null) {
+            content.append(inputLine);
+        }
+        in.close();
+        con.disconnect();
+
+        System.out.println(content);
+
+
+    }
+
+    /**
+     * Get Request
+     **/
+    @RestController
+    public class restController {
+
+        @GetMapping("/hello")
+        public String getHello() {
+            return "Hello";
+        }
+
+        /**
+         * Post Request
+         **/
+        @RestController
+        public class postHello {
+            @PostMapping("/hello")
+            public String postHello() {
+                return "Hello";
+            }
+        }
+    }
 }
--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,13 +1,38 @@
 package com.example.demo;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockMultipartFile;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.multipart;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
 class DemoApplicationTests {
 
+	@Autowired
+	private WebApplicationContext webApplicationContext;
+
 	@Test
-	void contextLoads() {
+	public void whenFileUploaded_thenVerifyStatus()
+			throws Exception {
+		MockMultipartFile file
+				= new MockMultipartFile(
+				"file",
+				"hello.txt",
+				MediaType.TEXT_PLAIN_VALUE,
+				"Hello, World!".getBytes()
+		);
+
+		MockMvc mockMvc
+				= MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
+		mockMvc.perform(multipart("/hello").file(file))
+				.andExpect(status().isOk());
 	}
 
 }
